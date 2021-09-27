752b278bd7953205e9dbb29039e8e749c8209b13
bossman-codex <56489079+bossman-codex@users.noreply.github.com>
2021-09-27 15:30:39 +0100

    shock

--- src/Component/LogIn.js
+++ src/Component/LogIn.js
@@ -51,7 +51,8 @@ function LogIn() {
       var user = result.user;
       console.log(user)
     })
-          }
+  }
+  
   useEffect(() => {
   firebase.auth().onAuthStateChanged( _user =>{
       if (_user ){
